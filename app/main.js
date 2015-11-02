35af2bf35a8543d257bf1d3640f164b806a68814
Zachary Huff <zach.huff.386@gmail.com>
2015-11-01 22:12:31 -0500

    Remove js files

--- app/main.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="references.d.ts" />
-var react = require('react');
-var test = require('components/test');
-react.render(react.createElement(test.Test, {
-    title: 'React Boilerplate',
-}), document.getElementById('app'));
-//# sourceMappingURL=main.js.map
