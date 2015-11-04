5134f737db571e145397b60e31dbc8a9e3507ac4
Zachary Huff <zach.huff.386@gmail.com>
2015-11-04 09:29:59 -0500

    Rename app

---
+++ app/App.tsx
@@ -0,0 +1,8 @@
+/// <reference path="References.d.ts" />
+import * as React from 'react';
+import List from './components/List';
+
+React.render(
+	<List title="React Polymer"/>,
+	document.getElementById('app')
+);
--- app/main.tsx
+++
@@ -1,8 +0,0 @@
-/// <reference path="references.d.ts" />
-import * as React from 'react';
-import Test from './components/test';
-
-React.render(
-	<Test title="React Polymer"/>,
-	document.getElementById('app')
-);
