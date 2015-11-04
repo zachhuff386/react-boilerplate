c768107ce09db81ee3e731739b7af008c90e4198
Zachary Huff <zach.huff.386@gmail.com>
2015-11-04 09:28:48 -0500

    Add types

---
+++ app/types/ItemType.ts
@@ -0,0 +1,10 @@
+/// <reference path="../References.d.ts" />
+
+export const CREATE = 'item_create';
+export const REMOVE = 'item_remove';
+export const UPDATE = 'item_update';
+
+export interface Item {
+	id: string;
+	content: string;
+}
