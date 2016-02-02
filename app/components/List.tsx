0eb3c4b1d11a31ac2cd93880881d7b75ca0b527a
Zachary Huff <zach.huff.386@gmail.com>
2016-02-02 01:06:58 -0500

    Add google map

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -29,6 +29,11 @@ const css = {
 		margin: '4px',
 		fontSize: '24px',
 	},
+	map: {
+		margin: '20px',
+		width: '250px',
+		height: '250px',
+	},
 };
 
 export default class List extends React.Component<Props, State> {
@@ -81,6 +86,18 @@ export default class List extends React.Component<Props, State> {
 			<ul>
 				{itemsDom}
 			</ul>
+			<google-map style={css.map} latitude="37.779" longitude="-122.3892" min-zoom="9" max-zoom="11"
+				language="en">
+				<google-map-marker latitude="37.779" longitude="-122.3892"
+					title="Go Giants!" draggable="true" drag-events>
+					<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/San_Francisco_Giants_Cap_Insignia.svg/200px-San_Francisco_Giants_Cap_Insignia.svg.png" />
+				</google-map-marker>
+				<google-map-poly closed fill-color="red" fill-opacity=".25" stroke-weight="1">
+					<google-map-point latitude="37.779" longitude="-122.3892"/>
+					<google-map-point latitude="37.804" longitude="-122.2711"/>
+					<google-map-point latitude="37.386" longitude="-122.0837"/>
+				</google-map-poly>
+			</google-map>
 		</div>;
 	}
 }
