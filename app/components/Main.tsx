dbf380b69a7f6e2dd4d7927b416629d316ca7e87
Zachary Huff <zach.huff.386@gmail.com>
2016-11-27 22:58:44 -0500

    Add alerts component

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -3,13 +3,17 @@ import * as React from 'react';
 import Styles from '../Styles';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import List from './List';
+import Alerts from './Alerts';
 
 document.body.style.backgroundColor = Styles.colors.background;
 
 export default class Main extends React.Component<void, void> {
 	render(): JSX.Element {
 		return <MuiThemeProvider muiTheme={Styles.theme}>
-			<List title="React Polymer"/>
+			<div>
+				<List title="React Polymer"/>
+				<Alerts/>
+			</div>
 		</MuiThemeProvider>;
 	}
 }
