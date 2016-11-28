9a7b874e423b109d7c438b098e5916c4d8b1c38b
Zachary Huff <zach.huff.386@gmail.com>
2016-11-27 19:57:15 -0500

    Add error types

--- app/types/ErrorTypes.ts
+++ app/types/ErrorTypes.ts
@@ -2,10 +2,22 @@
 export const CREATE = Symbol('error.create');
 export const REMOVE = Symbol('error.remove');
 
+export const WARNING = Symbol('error.warning');
+export const ERROR = Symbol('error.error');
+
 export interface Error {
 	id?: string;
 	level?: Symbol;
 	message?: string;
 }
 
-export type Errors = {[key: string]: Error};
+export type Errors = Error[];
+
+export interface ErrorDispatch {
+	type: Symbol;
+	data?: {
+		id?: string;
+		level?: Symbol;
+		message?: string;
+	};
+}
